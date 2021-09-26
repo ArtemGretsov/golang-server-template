942fde58d78a05b053cf0a160795f9edfb2a2a2e
Artem Gretsov <agreczov@gmail.com>
2021-09-26 22:13:31 +0300

    custom auth middleware

--- src/middlewares/authmw/authmw.go
+++ src/middlewares/authmw/authmw.go
@@ -2,6 +2,9 @@ package authmw
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"regexp"
+
+	"github.com/ArtemGretsov/golang-server-template/src/tools/errorstool"
 )
 
 type JWTPayload struct {
@@ -11,6 +14,18 @@ type JWTPayload struct {
 }
 
 func Middleware(ctx *fiber.Ctx) error {
+	authorizationHeader := ctx.Request().Header.Peek("Authorization")
+	token := regexp.
+		MustCompile(`Bearer (.+)`).
+		ReplaceAllString(string(authorizationHeader), `$1`)
+
+	jwtPayload, err := ParseJWT(token)
+	if err != nil {
+		return errorstool.NewHTTPError(fiber.StatusUnauthorized, "unauthorized")
+	}
+
+	ctx.Locals("user", &jwtPayload)
+
 	return ctx.Next()
 }
 
--- src/modules/auth/controller.go
+++ src/modules/auth/controller.go
@@ -1,16 +1,15 @@
 package auth
 
 import (
-	"github.com/ArtemGretsov/golang-server-template/src/middlewares/authmw"
-	"github.com/ArtemGretsov/golang-server-template/src/middlewares/validationmw"
 	"github.com/gofiber/fiber/v2"
+
+	"github.com/ArtemGretsov/golang-server-template/src/middlewares/validationmw"
 )
 
 func Controller(r fiber.Router) {
   router := r.Group("/auth")
 	router.Post(
 			"/signup",
-			authmw.Middleware,
 			validationmw.ValidateBodyMiddleware(SignupReqDto{}),
 			Service.Signup,
 	)
